3960fd1546859c1a7b7b7f4e54a3a81eb49d109c
Doohyun <qorengus531@bizmeka.com>
2026-07-15 14:50:55 +0900

    fix: 경로 alias 설정

--- musicnote-store-frontend/vite.config.ts
+++ musicnote-store-frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,7 +9,20 @@ export default defineConfig({
     vue(),    
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-vue-next'],
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      },
+    }
+  }
 })
